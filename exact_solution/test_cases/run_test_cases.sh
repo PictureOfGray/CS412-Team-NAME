f7b7d10eee35c2eec11b863fd2a13ca13d94f063
jay6o <richforeverrr6@proton.me>
2025-11-14 11:43:16 -0500

    Fixed old test file name being used in run_test_cases.sh

--- exact_solution/test_cases/run_test_cases.sh
+++ exact_solution/test_cases/run_test_cases.sh
@@ -1,4 +1,4 @@
 python3 ../cs412_tsp_exact.py < ./abc.txt
-python3 ../cs412_tsp_exact.py < ./abcdefg.txt
+python3 ../cs412_tsp_exact.py < ./abcdef.txt
 
 # Clearly label (with comments) which test case causes your program to run for more than 20 minutes.
