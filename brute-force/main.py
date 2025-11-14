46b237736e36f59204a9e078bacdb2f0c4bdf17d
jay6o <richforeverrr6@proton.me>
2025-11-14 10:22:50 -0500

    Brute force/exact solution for the problem complete (i think)

--- brute-force/main.py
+++ brute-force/main.py
@@ -1,5 +1,43 @@
+"""
+    Brute force algorithm for finding the shortest path
+    in the traveling salesperson problem.
+"""
+
+
+from itertools import permutations
 def main():
-    return
+    G = {}
+    nodes = set()
+    v, e = input().strip().split()
+    v, e = int(v), int(e)
+
+    # Build our graph using the input
+    for _ in range(v):
+        v1, v2, e = input().split()
+        if v1 not in G:
+            G[v1] = {}
+        if v2 not in G:
+            G[v2] = {}
+        G[v1][v2] = float(e)
+        G[v2][v1] = float(e)
+        nodes.add(v1)
+        nodes.add(v2)
+    
+    # Try all permutations of the paths to find a shortest distance path
+    node_permutations = permutations(nodes)
+    best = float('inf')
+    for perm in node_permutations:
+        curr = 0
+        for i in range(len(perm)):
+            if i == len(perm) - 1:
+                curr += G[perm[i]][perm[0]]
+            else:
+                curr += G[perm[i]][perm[i + 1]]
+        best = min(curr, best)
+        best_path = perm + (perm[0],)
+    print(f"{best:.4f}\n{" ".join(best_path)}")
+
+## Approximation Algorithm: iterative improvement
 
 if __name__ == "__main__":
     main()
