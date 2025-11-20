d2771265ab6cc38a129cc915d947ee02a6bbfb54
jay6o <richforeverrr6@proton.me>
2025-11-20 12:16:45 -0500

    Added time complexity comments

--- exact_solution/cs412_transformation.py
+++ exact_solution/cs412_transformation.py
@@ -1,8 +1,17 @@
+# Make an input graph to the hamiltonian cycle problem complete
 def hamiltonian_to_tsp(G, V):
-    # Make the graph complete in polynomial time
+
+    # O(V^2)
+    for v in G:                             # O(V)
+        for u in V:                         # O(V)
+            if u not in G[v] and u != v:    # O(1) 
+                G[v][u] = float('inf')      # O(1)
     return
 
+
+
 def main():
+
     n = int(input())
     G = {}
     V = set()
@@ -10,9 +19,12 @@ def main():
         v1, v2, w = input().strip().split()
         if v1 not in G:
             G[v1] = {}
+        if v2 not in G:
+            G[v2] = {}
         V.add(v1)
         V.add(v2)
         G[v1][v2] = float(w)
+        G[v2][v1] = float(w)
     tsp = hamiltonian_to_tsp(G, V)
     return
 
